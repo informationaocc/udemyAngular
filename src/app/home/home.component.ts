949ccc55a077657134596ea1d4f72046f941d57e
informationaocc <102293769+informationaocc@users.noreply.github.com>
2022-04-15 17:13:29 +0530

    directives learning

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,14 +3,23 @@ import {Component, OnInit} from '@angular/core'
 @Component({
     selector:'.app-home',
     templateUrl:'home.component.html',
-    styleUrls:['home.component.css']
+    styles:[`
+    .online{
+        color:white;
+    }
+    `]
 })
 export class HomeComponent implements OnInit{
     btnStatus:boolean=false;
     serverCreateStatus:string='No Server was Created';
     serverName:string='TestServer';
+    serverCreated:boolean=false;
+    serverStatus:string='offline';
+    serverID:number=10;
+    servers=['Testserver','Testserver 2' ]
+    constructor(){   
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';    
 
-    constructor(){        
         setTimeout(()=>{
             this.btnStatus=true;
         },3000) 
@@ -21,9 +30,18 @@ export class HomeComponent implements OnInit{
     }
     
     onCreateServer(){
+        this.serverCreated=true;
+        this.servers.push(this.serverName);
         this.serverCreateStatus='Server has created. Name is '+ this.serverName;
+        this.serverStatus='online';
     }
     onUpdateServerName(event:Event){
         this.serverName=(<HTMLInputElement>event.target).value;
+    }
+    getserverStatus(){
+        return this.serverStatus;
+    }
+    getColor(){
+        return this.serverStatus==='online'? 'green':'red';
     }
 }
